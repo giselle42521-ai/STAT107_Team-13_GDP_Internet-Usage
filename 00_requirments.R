5f396a9f06d2ec07b6c7f739507f75b378c6a727
giselle42521-ai <giselle42521@gmail.com>
2025-12-05 12:18:01 -0800

    Update 00_requirments.R

--- 00_requirments.R
+++ 00_requirments.R
@@ -1,3 +1,9 @@
 Libraries : 
   
 library(tidyverse)
+
+Libraries used to do more cleaning of both the GDP and Internet Users Excel file:
+library(readxl)
+library(dplyr)
+library(janitor)
+library(writexl)
